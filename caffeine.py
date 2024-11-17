3a6c8821eb1f27bd492c7ad5aee6c086d3926e28
Brown <Brock.Brown@SignetJewelers.com>
2024-11-17 02:08:38 -0500

    Fixed caffeine (weird)

--- caffeine.py
+++ caffeine.py
@@ -1,11 +1,11 @@
 import pyautogui
 import time
 
-def press_f15_every_10_minutes()
-    while True
+def press_f15_every_10_minutes():
+    while True:
         pyautogui.press('f15')  # Simulate pressing F15
-        print(F15 key pressed.)
+        print("F15 key pressed.")
         time.sleep(600)  # Wait for 600 seconds (10 minutes)
 
-if __name__ == __main__
+if __name__ == "__main__":
     press_f15_every_10_minutes()
