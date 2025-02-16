9a6fad8f747e8045706db60091f16c4070168943
Brown <Brock.Brown@SignetJewelers.com>
2025-02-16 00:22:20 -0500

    Added option for must_start_alarm

--- opsgenie.js
+++ opsgenie.js
@@ -519,8 +519,8 @@ async function fillOpsGenieAlert() {
     } else if (status === "Terminated (TERMINATED)") {
         status = "JOBTERMINATED";
     } else if (status === "Success (SUCCESS)") {
-        alert("Heads up, it's successful already. Defaulting status to MAXRUNALARM.");
-        status = "MAXRUNALARM";
+        const userChoice = confirm("Click OK for MAX RUN ALARM, Cancel for MUST START ALARM");
+        status = userChoice ? "MAXRUNALARM" : "MUST_START_ALARM";
     } else if (status === "") {
         alert("Status not found. Defaulting status to MAXRUNALARM.");
         status = "MAXRUNALARM";
@@ -590,7 +590,7 @@ async function fillOpsGenieAlert() {
 
 function copyTextToClipboard(text) {
     var textArea = document.createElement("textarea");
-  
+
     //
     // *** This styling is an extra step which is likely not required. ***
     //
@@ -606,35 +606,35 @@ function copyTextToClipboard(text) {
     // box asking the user for permission for the web page to
     // copy to the clipboard.
     //
-  
+
     // Place in the top-left corner of screen regardless of scroll position.
     textArea.style.position = 'fixed';
     textArea.style.top = 0;
     textArea.style.left = 0;
-  
+
     // Ensure it has a small width and height. Setting to 1px / 1em
     // doesn't work as this gives a negative w/h on some browsers.
     textArea.style.width = '2em';
     textArea.style.height = '2em';
-  
+
     // We don't need padding, reducing the size if it does flash render.
     textArea.style.padding = 0;
-  
+
     // Clean up any borders.
     textArea.style.border = 'none';
     textArea.style.outline = 'none';
     textArea.style.boxShadow = 'none';
-  
+
     // Avoid flash of the white box if rendered for any reason.
     textArea.style.background = 'transparent';
-  
-  
+
+
     textArea.value = text;
-  
+
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
-  
+
     try {
       var successful = document.execCommand('copy');
       var msg = successful ? 'successful' : 'unsuccessful';
@@ -642,7 +642,7 @@ function copyTextToClipboard(text) {
     } catch (err) {
       console.log('Oops, unable to copy');
     }
-  
+
     document.body.removeChild(textArea);
   }
 
