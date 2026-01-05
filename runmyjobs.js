423b79fde47715d25cc83fb413bb7f02fc566b49
Brown <Brock.Brown@SignetJewelers.com>
2026-01-05 07:43:47 -0500

    abap detection works better

--- runmyjobs.js
+++ runmyjobs.js
@@ -142,7 +142,7 @@ async function copyMatchingJobsAboveSelected() {
                     let jobName = cells[definitionIndex].textContent.trim();
                     jobName = jobName.split(' ')[0];
                     uniqueJobNames.add(jobName);
-                    if (jobName.endsWith("_ABAP")) {
+                    if (jobName.split("_").pop().startsWith("ABAP")) {
                         alert("ABAP job found, make sure that goes into DevOps chat. :)")
                     }
                 }
