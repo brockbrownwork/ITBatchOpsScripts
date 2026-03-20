2c39613e104fc1ad5ed1b5afcd6163152698fa20
Brock Brown <BBrown@jewels.com>
2026-03-19 23:23:29 -0400

    do the pull before you run the scripts

--- 00_run_all_numbered.bat
+++ 00_run_all_numbered.bat
@@ -1,7 +1,6 @@
-@echo off
+git pull
 REM Runs all numbered batch and python files in separate cmd windows
 
-start "0 - Check for Updates" cmd /c "0_check_for_updates.bat"
 start "1 - Caffeine" cmd /c "python 1_caffeine.py"
 start "2 - Critical Job Reminders" cmd /c "python 2_critical_job_reminders.py"
 start "3 - Susejboot" cmd /c "3_Susejboot.bat"
